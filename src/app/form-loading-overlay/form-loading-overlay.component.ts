7fc07eeb401c06257402db08fd7757a6f284456a
rock-bolton-hyland <rock.bolton@hyland.com>
2020-03-13 13:36:56 -0400

    3 files modified, 1 deleted

--- src/app/form-loading-overlay/form-loading-overlay.component.ts
+++ src/app/form-loading-overlay/form-loading-overlay.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnChanges, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'hy-form-loading-overlay',
   templateUrl: './form-loading-overlay.component.html',
-  styleUrls: ['./form-loading-overlay.component.css']
+  styleUrls: ['./form-loading-overlay.component.css'],
+  host:     {'[class.visible]':'visible'}
 })
-export class FormLoadingOverlayComponent implements OnInit {
+export class FormLoadingOverlayComponent implements AfterViewInit, OnChanges {
+  @Input() visible = false;
 
-  constructor() { }
+  constructor(private _renderer: Renderer2, private _host: ElementRef) { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    const parent = this._renderer.parentNode(this._host.nativeElement);
+    this._renderer.setStyle(parent, 'position', 'relative');
+    console.log('parent = ' + parent);
+    //this._host.nativeElement.wdith = '100px';
+    // this._renderer.setStyle(this._host.nativeElement, 'width', parent.nativeElement.width );
+    
   }
 
+  ngOnChanges(){
+    // Set host element width and height to prent element width and height;
+    const parent = this._renderer.parentNode(this._host);
+    console.log('parent = ' + parent);
+  }
 }
